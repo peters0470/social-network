3a4d68267db108214486699314a0704bb47428e7
unknown <peters0470@gmail.com>
2021-02-13 17:41:23 -0600

    set up crud for user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,17 @@
 const { User } = require('../models');
 
 const userController = {
-    //get all
     getAllUsers(req, res) {
         User.find({})
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id}) 
             .then(dbUserData => res.json(dbUserData))
             .catch(err => {
                 console.log(err);
@@ -15,6 +23,32 @@ const userController = {
         console.log( body)
         User.create(body)
             .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
+
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id },  body, { new: true})
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({
+                        message: 'No user with this id!'
+                    })
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id   })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({  message: 'no user found with this id'  });
+                    return;
+                }
+                res.json(dbUserData);
+            })
             .catch(err => res.status(400).json(err));
     },
 }
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,12 +1,23 @@
 const router = require('express').Router();
 const {
     getAllUsers,
-    createUser
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser
+
 } = require('../../controllers/user-controller')
 
 router
     .route('/')
     .get(getAllUsers)
     .post(createUser)
+    
+
+router
+    .route('/:id')
+    .get(getUserById)  
+    .put(updateUser)  
+    .delete(deleteUser)
 
 module.exports = router;   
